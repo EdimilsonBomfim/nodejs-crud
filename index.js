6d8a86b85acdf6f8dd5704c4e325d66cd0b24733
EBOMFIM-exemplos <ebomfim.exemplos@gmail.com>
2022-07-01 12:47:39 -0300

    atualização apos configurar login e senha no mongoDB

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //config inicial
 const express = require('express')
+const mongoose = require('mongoose') //importat mongoose
 const app = express()
 
 //formade ler JSON / middlewares
@@ -21,4 +22,16 @@ app.get('/', (req, res) =>{
 } )
 
 // entregar uma porta
-app.listen(3000)
+const DB_USER = 'edimilson'
+const DB_PASSWORD= encodeURIComponent('ordVybCaA7oBw7eW')
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.4fvwlaw.mongodb.net/bancotesteapi?retryWrites=true&w=majority´`
+
+    )   
+    . then(() =>{
+        console.log("Conectamos ao MongoDB!")
+        app.listen(3000)
+    })  
+    .catch((err) => console.log(err)) 
+
